e2c6ae6bfb5f535208fb41ed5da608e83a494912
Daria <zelya2909@gmail.com>
2024-04-04 19:52:43 -0400

    styling

--- hacker-snooze/js/stories.js
+++ hacker-snooze/js/stories.js
@@ -22,32 +22,38 @@ async function getAndShowStoriesOnStart() {
 function generateStoryMarkup(story, isMyStoryPage = false) {
   console.debug("generateStoryMarkup", story);
 
-  let favStory
+  let favStory;
   const hostName = story.getHostName();
 
   if (!currentUser) {
-    favStory = false
+    favStory = false;
   } else {
-    favStory = currentUser.isStoryInFavorites(story.storyId)
+    favStory = currentUser.isStoryInFavorites(story.storyId);
   }
 
-  const starType = favStory ? "fas" : "far"
-  const isStarHidden = !currentUser
+  const starType = favStory ? "fas" : "far";
+  const isStarHidden = !currentUser;
 
-  console.log(isStarHidden)
+  console.log(isStarHidden);
 
   return $(`
-      <li id="${story.storyId}">
-        ${isMyStoryPage ? `<span class="delete"><i class="fas fa-ban icon-hover"></i></span>` : ''}
-        <span class="star" style="display:${isStarHidden ? 'none' : 'inline'};">
+      <li class ="story-item" id="${story.storyId}">
+        ${
+          isMyStoryPage
+            ? `<span class="delete"><i class="fas fa-ban icon-hover"></i></span>`
+            : ""
+        }
+        <span class="star" style="display:${isStarHidden ? "none" : "inline"};">
           <i class="${starType} fa-star"></i>
         </span>
+        <div class="story-details">
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
         <small class="story-hostname">(${hostName})</small>
         <small class="story-author">by ${story.author}</small>
         <small class="story-user">posted by ${story.username}</small>
+        <div>
       </li>
     `);
 }
@@ -69,39 +75,38 @@ function putStoriesOnPage() {
 }
 
 function putMyFavoriteStoriesOnPage() {
-  console.debug("putMyFavoriteStotiesOnPage")
-  $favoriteStoriesList.empty()
+  console.debug("putMyFavoriteStotiesOnPage");
+  $favoriteStoriesList.empty();
 
-  const favStories = currentUser.favorites
+  const favStories = currentUser.favorites;
 
   if (!favStories || favStories.length === 0) {
-    $favoriteStoriesList.append("<h5>No favorites added!</h5>")
+    $favoriteStoriesList.append("<h5>No favorites added!</h5>");
   } else {
     for (let story of favStories) {
-      const $story = generateStoryMarkup(story)
-      $favoriteStoriesList.append($story)
+      const $story = generateStoryMarkup(story);
+      $favoriteStoriesList.append($story);
     }
   }
 
-  $favoriteStoriesList.show()
+  $favoriteStoriesList.show();
 }
 
-
 function putMyStoriesOnPage() {
-  console.debug("putMyStoriesOnPage")
+  console.debug("putMyStoriesOnPage");
 
-  $myStoriesList.empty()
+  $myStoriesList.empty();
 
-  const myStories = currentUser.ownStories
+  const myStories = currentUser.ownStories;
 
-  console.debug(myStories)
+  console.debug(myStories);
 
   if (!myStories || myStories.length === 0) {
     $myStoriesList.append("<h5>No stories added by user yet!</h5>");
   } else {
     for (let story of myStories) {
-      const $story = generateStoryMarkup(story, true)
-      $myStoriesList.append($story)
+      const $story = generateStoryMarkup(story, true);
+      $myStoriesList.append($story);
     }
   }
 
@@ -118,9 +123,9 @@ async function CreateNewStory(evt) {
   const author = $("#create-author").val();
 
   // get username from current user
-  const username = currentUser.username
+  const username = currentUser.username;
   // create story data object
-  const storyData = {title, url, author, username };
+  const storyData = { title, url, author, username };
 
   // Call API to create new story
   const story = await storyList.addStory(currentUser, storyData);
@@ -138,36 +143,36 @@ async function CreateNewStory(evt) {
 
 $submitForm.on("submit", CreateNewStory);
 
-
 async function setStoryAsFavorite(evt) {
-  console.debug("setStoryAsFavorite", evt)
+  console.debug("setStoryAsFavorite", evt);
   const star = evt.currentTarget.children[0];
-  const classList = star.classList
+  const classList = star.classList;
 
-  const storyId = evt.currentTarget.parentNode.id
-  const story = storyList.getStoryById(storyId)
+  const storyId = evt.currentTarget.parentNode.id;
+  const story = storyList.getStoryById(storyId);
 
-  if (classList.contains("far")) {  // "far" is the class when the story is not favorited
-    classList.replace("far", "fas")
-    await currentUser.addStoryToFavorites(story)
+  if (classList.contains("far")) {
+    // "far" is the class when the story is not favorited
+    classList.replace("far", "fas");
+    await currentUser.addStoryToFavorites(story);
   } else {
-    classList.replace("fas", "far")
-    await currentUser.removeStoryFromFavoritesById(storyId)
+    classList.replace("fas", "far");
+    await currentUser.removeStoryFromFavoritesById(storyId);
   }
 
-  star.classList = classList
+  star.classList = classList;
 }
 
 $allStoriesList.on("click", ".star", setStoryAsFavorite);
 $favoriteStoriesList.on("click", ".star", setStoryAsFavorite);
 
 async function deleteFromMyStories(evt) {
-  console.debug("deleteFromMyStories", evt)
-  const storyId = evt.currentTarget.parentNode.id
+  console.debug("deleteFromMyStories", evt);
+  const storyId = evt.currentTarget.parentNode.id;
 
-  await storyList.deleteStory(currentUser, storyId)
+  await storyList.deleteStory(currentUser, storyId);
 
   putMyStoriesOnPage();
 }
 
-$myStoriesList.on("click", ".delete", deleteFromMyStories)
+$myStoriesList.on("click", ".delete", deleteFromMyStories);
