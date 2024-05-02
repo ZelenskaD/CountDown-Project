ae70c36169b3501aec19b5e1a2a9da2e18cd4d6b
Daria <zelya2909@gmail.com>
2024-05-02 15:16:47 -0400

    sql ex join

--- PYTHON/pythonRunFiles/py_practice/py_paking_un/people.py
+++
@@ -1,8 +0,0 @@
-from helpers import get_rand_month, get_rand_year
-def make_person(name, favCOlor):
-  return {
-    'name': name,
-    'favColor': favColor,
-    'bitrh_year': get_rand_year(),
-    'birth_month': get_rand_month()
-  }
